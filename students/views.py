af2da21d7a36399901e5e756ffb85708022752ea
ofisser86 <ofisser86@gmail.com>
2017-08-18 18:34:35 +0200

    Add groups and students dynamic url

--- students/views.py
+++ students/views.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+# Students views.
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+
+
+def students_add(request):
+    return HttpResponse('<h1> Student Add Form</h1>')
+
+
+def students_edit(request, sid):
+    return HttpResponse('<h1> Student %s Editing </h1>' % sid)
+
+
+def students_delete(request, sid):
+    return HttpResponse('<h1> Student %s deleting </h1>' % sid)
+
+
+# Students views.
+def groups_list(request):
+    return HttpResponse('<h1> Groups list </h1>')
+
+
+def groups_add(request):
+    return HttpResponse('<h1> Groups Add Form </h1>')
+
+
+def groups_edit(request, gid):
+    return HttpResponse('<h1> Groups %s Editing </h1>' % gid)
+
+
+def groups_delete(request, gid):
+    return HttpResponse('<h1> Groups deleting %s</h1>' % gid)
