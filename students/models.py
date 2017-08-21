5028591aca70eaf0da0316d6680248312a555741
ofisser86 <ofisser86@gmail.com>
2017-08-21 13:48:18 +0200

    add Meta class to Student model

--- students/models.py
+++ students/models.py
@@ -8,6 +8,10 @@ from django.db import models
 class Student(models.Model):
     """Student model"""
 
+    class Meta(object):
+        verbose_name = u'Студент'
+        verbose_name_plural = u'Студенти'
+
     first_name = models.CharField(max_length=256, blank=False, verbose_name=u'Імя')
     last_name = models.CharField(max_length=256, blank=False, verbose_name=u'Прізвище')
     middle_name = models.CharField(max_length=256, blank=True, verbose_name=u'По-бфтькові')
@@ -15,3 +19,6 @@ class Student(models.Model):
     photo = models.ImageField(blank=True, null=True, verbose_name=u'Фото')
     ticket = models.CharField(max_length=256, blank=True, verbose_name=u'Білет')
     notes = models.TextField(blank=True, verbose_name=u'Нотатки')
+
+    def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
