bac1784fe972e98b3548754db57abe6b648bdbc5
ofisser86 <ofisser86@gmail.com>
2017-08-25 17:00:48 +0200

    add dynamic pagination

--- students/views/students.py
+++ students/views/students.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from ..models import Student
 
@@ -24,6 +25,20 @@ def students_list(request):
         students = students.order_by(order_by)
         if request.GET.get('reverse', '') == '1':
             students = students.reverse()
+
+    # paginate students
+    paginator = Paginator(students, 5)
+    page = request.GET.get('page')
+
+    try:
+        students = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        students = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver
+        # last page of results.
+        students = paginator.page(paginator.num_pages)
 
     # import pdb;pdb.set_trace()
     # return HttpResponse("Hello")
