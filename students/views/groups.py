cc17388dc2f6a633fadedc490c63a77904f03c78
ofisser86 <ofisser86@gmail.com>
2017-08-26 10:36:05 +0200

    add paginator and sorting for groups

--- students/views/groups.py
+++ students/views/groups.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Groups views.
 from ..models import Group
@@ -10,6 +11,25 @@ from ..models import Group
 
 def groups_list(request):
     groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        groups = paginator.page(paginator.num_pages)
 
     return render(request, 'students/groups.html', {'groups': groups})
 
