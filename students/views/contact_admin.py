d90345ffa0f4f1d536ef5dee7238830447f549bf
ofisser86 <ofisser86@gmail.com>
2017-09-02 14:18:34 +0200

    add class ContactForm

--- students/views/contact_admin.py
+++ students/views/contact_admin.py
@@ -3,6 +3,13 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django import forms
+
+
+class ContactForm(forms.Form):
+    form_email = forms.EmailField(label=u'Ваша Емейл Адреса')
+    subject = forms.CharField(max_length=128, label=u'Заголовок листа')
+    message = forms.CharField(max_length=2650, label=u'Текст повідомлення', widget=forms.Textarea)
 
 
 def contact_admin(request):
