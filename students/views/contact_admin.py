1a724fadefa40a1299bf0499718d88f2411f7c36
ofisser86 <ofisser86@gmail.com>
2017-09-03 16:10:46 +0200

    add  ContactForm views logic

--- students/views/contact_admin.py
+++ students/views/contact_admin.py
@@ -1,17 +1,44 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django import forms
+from django.urls import reverse
+
+from studentsdb.settings import ADMIN_EMAIL
 
 
 class ContactForm(forms.Form):
-    form_email = forms.EmailField(label=u'Ваша Емейл Адреса')
+    from_email = forms.EmailField(label=u'Ваша Емейл Адреса')
     subject = forms.CharField(max_length=128, label=u'Заголовок листа')
     message = forms.CharField(max_length=2650, label=u'Текст повідомлення', widget=forms.Textarea)
 
 
 def contact_admin(request):
-    return render(request, 'contact_admin/contact_admin.html', {})
+    # check if form was posted
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request
+        form = ContactForm(request.POST)
+        # check whether user data is valid:
+        if form.is_valid():
+            # send email
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            from_email = form.cleaned_data['from_email']
+
+            try:
+                send_mail(subject, message, from_email, [ADMIN_EMAIL])
+            except Exception:
+                message = u'Під час відправки листа виникла непередбачувана помилка. Спробуйте пізніше'
+            else:
+                message = u'Повідомлення успішно надіслане!'
+        # redirect to same contact page with success message
+        return HttpResponseRedirect(u'%s?status_message=%s' % (reverse('contact_admin'), message))
+    # if there was not POST render blank form
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact_admin/contact_admin.html', {'form': form})
 
