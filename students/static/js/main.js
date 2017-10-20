523ab0af2cae28be4ece4b98f65abfd0a2821b75
ofisser86 <ofisser86@gmail.com>
2017-10-20 15:54:16 +0800

    add error jquery message and change class to danger

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -1,5 +1,6 @@
 function initJournal() {
     var indicator = $('#ajax-progress-indicator');
+    var ajaxerror = $('#errJournalSave').hide();
     $('.day-box input[type="checkbox"]').click(function (event) {
         var box = $(this);
         $.ajax(box.data('url'), {
@@ -14,13 +15,15 @@ function initJournal() {
            },
             'beforeSend': function (xhr, settings) {
                 indicator.show();
+                ajaxerror.hide();
             },
             'error': function (xhr, status, error) {
-               alert(error);
                indicator.hide();
+               ajaxerror.show().addClass('.alert alert-danger').append(error);
             },
             'success': function (data, status, xhr) {
                indicator.hide();
+               ajaxerror.hide();
             }
         });
     });
