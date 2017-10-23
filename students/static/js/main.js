feb9c9b02fc5400f28c2910bc557caac20583062
ofisser86 <ofisser86@gmail.com>
2017-10-23 17:23:45 +0800

    create initEditStudentForm in main.js for modal window edit student. Send data in ajax mode

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -4,26 +4,26 @@ function initJournal() {
     $('.day-box input[type="checkbox"]').click(function (event) {
         var box = $(this);
         $.ajax(box.data('url'), {
-           'type': 'POST',
-           'async':true,
-           'dataType': 'json',
-           'data':{
-               'pk':box.data('student-id'),
-               'date':box.data('date'),
-               'present':box.is(':checked') ? '1' : '',
-               'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
-           },
+            'type': 'POST',
+            'async': true,
+            'dataType': 'json',
+            'data': {
+                'pk': box.data('student-id'),
+                'date': box.data('date'),
+                'present': box.is(':checked') ? '1' : '',
+                'csrfmiddlewaretoken': $('input[name="csrfmiddlewaretoken"]').val()
+            },
             'beforeSend': function (xhr, settings) {
                 indicator.show();
                 ajaxerror.hide();
             },
             'error': function (xhr, status, error) {
-               indicator.hide();
-               ajaxerror.show().addClass('.alert alert-danger').append(error);
+                indicator.hide();
+                ajaxerror.show().addClass('.alert alert-danger').append(error);
             },
             'success': function (data, status, xhr) {
-               indicator.hide();
-               ajaxerror.hide();
+                indicator.hide();
+                ajaxerror.hide();
             }
         });
     });
@@ -36,23 +36,23 @@ function initGroupSelection() {
         // get value of currently selected group option
         var group = $(this).val()
 
-        if (group){
+        if (group) {
             // set cookie with expiration date 1 year since now;
             // cookie creation function takes period in days
-            $.cookie('current_group', group, {'path':'/', 'expires':365});
+            $.cookie('current_group', group, {'path': '/', 'expires': 365});
         }
         else {
             // otherwise we delete the cookie
             $.removeCookie('current_group', {'path': '/'});
         }
-            // and reload a page
-            location.reload(true);
+        // and reload a page
+        location.reload(true);
 
-            return true;
+        return true;
     });
 }
 
-function  initDateFields() {
+function initDateFields() {
     $('input.dateinput').datetimepicker({
         'format': 'YYYY-MM-DD'
     }).on('dp.hide', function (event) {
@@ -64,12 +64,12 @@ function initEditStudentPage() {
     $('a.student-edit-form-link').click(function (event) {
         var link = $(this);
         $.ajax({
-            'url':link.attr('href'),
+            'url': link.attr('href'),
             'dataType': 'html',
-            'type':'get',
-            'success':function (data, status, xhr) {
+            'type': 'get',
+            'success': function (data, status, xhr) {
                 // check if we got successfull response from the server
-                if (status != 'success'){
+                if (status != 'success') {
                     alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
                     return false;
                 }
@@ -89,11 +89,53 @@ function initEditStudentPage() {
                 alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
                 return false;
             }
-            
+
         });
         return false;
     });
 }
+
+function initEditStudentForm(form, modal) {
+    // attach datepicker
+    initDateFields();
+
+    // close modal window on Cancel button click
+    form.find('input[name="cancel_button"]').click(function (event) {
+        modal.modal('hide');
+        return false;
+    });
+
+    // make form work in AJAX mode
+    form.ajaxForm({
+        'dataType': 'html',
+        'error': function () {
+            alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+            return false;
+        },
+        'success': function (data, status, xhr) {
+            var html = $(data), newform = html.find('#content-column form');
+
+            // copy alert to modal window
+            modal.find('.modal-body').html(html.find('.alert'));
+
+            // copy form to modal if we found it in server response
+            if (newform.length > 0) {
+                modal.find('.modal-body').append(newform);
+                // initialize form fields and buttons
+                initEditStudentForm(newform, modal);
+            } else {
+                // if no form, it means success and we need to reload page
+                // to get updated students list;
+                // reload after 2 seconds, so that user can read
+                // success message
+                setTimeout(function () {
+                    location.reload(true);
+                }, 500);
+            }
+        }
+    });
+}
+
 $(document).ready(function () {
     initJournal();
     initGroupSelection();
